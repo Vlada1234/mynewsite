7c5415e94a4e490ac4705b18c9fa2e97d48b6dc7
Vlada1234 <zivkovicv107@gmail.com>
2021-01-15 09:22:11 +0100

    test

---
+++ src/main/java/com/mynewsite/mynewsite/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.mynewsite.mynewsite.repository;
+
+import com.mynewsite.mynewsite.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+
+}
